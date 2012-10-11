c6c5d22a92b6e1ed8bd7df3ec692a537f05c0618
Milot Shala <milot.shala@gmail.com>
2012-10-11 18:50:57 +0200

    Simple list items

--- src/com/milot/example/list/MainActivity.java
+++ src/com/milot/example/list/MainActivity.java
@@ -14,7 +14,19 @@ public class MainActivity extends ListActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayList<String> lista = new ArrayList<String>();
+        lista.add("Hajdari");
+        lista.add("Hanifja");
+        lista.add("Xhemshiti");
+        lista.add("Hamiti");
+        lista.add("Bislimi");
+        lista.add("Basria");
         
+        ArrayAdapter<String> adapteri = new
+        		ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, lista);
+        
+        
+        setListAdapter(adapteri);
     }
 
     @Override
